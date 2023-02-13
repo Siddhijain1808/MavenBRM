8180e80dbad39b6fb82fc16d6daea47480511323
Siddhi <siddhijain1808@gmail.com>
2023-02-13 23:13:26 +0530

    First commit

---
+++ src/main/java/pkg4/MyClass.java
@@ -0,0 +1,15 @@
+package pkg4;
+
+public class MyClass {
+	public void show() {
+		System.out.println("Hello Devops! Welcome to jenkins");
+	}
+
+	public static void main(String[] args) {
+		MyClass ob=new MyClass();
+		ob.show();
+	
+
+	}
+
+}
